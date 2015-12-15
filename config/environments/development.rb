ce9e01a325cc9b82bad842cc8a962da6193923fb
stopanko <stepander007@gmail.com>
2015-12-15 17:05:37 +0200

    add smtp settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,20 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => 'mail.google.com',
+      :user_name            => 'stopankomail@gmail.com',
+      :password             => '86341421993',
+      :authentication       => 'plain'
+      # :enable_starttls_auto => true
+  }
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: Rails.application.routes.default_url_options[:host] }
+
 end
