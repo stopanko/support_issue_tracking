1fb77b157be0a979b4fadecd89a9db51384a04c7
stopanko <stepander007@gmail.com>
2015-12-16 18:28:14 +0200

    try add new ticket page and add bootstrap styles

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,4 +3,8 @@ class TicketsController < ApplicationController
   def index
 
   end
+
+  def new
+    @ticket = Ticket.new
+  end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,10 @@
 class Ticket < ActiveRecord::Base
+  before_create :set_default_status
+  validates_presence_of :user_name, :email, :title, :text, :problem_type_id, :status_id
+
+
+  private
+  def set_default_status
+
+  end
 end
