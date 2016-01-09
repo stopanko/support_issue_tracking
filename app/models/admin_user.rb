fa078e986553898fa3db079c26fdc666b5648668
stopanko <stepander007@gmail.com>
2016-01-10 00:04:04 +0200

    add admin default login and password

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,10 +1,17 @@
 class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  after_initialize :set_data
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :comments
 
   has_many :tickets
+
+  private
+  def set_data
+    self.email = "admin@example.com"
+    self.password = "password"
+  end
 end
